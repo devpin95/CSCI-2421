4f87fc387d69e28d77d6fe2a3863f2cc7998e4b2
devpin <devpindroid@gmail.com>
2017-04-12 22:43:35 -0600

    Implemented insert() and report()

--- HashTable.cpp
+++ HashTable.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "HashTable.h"
+
+HashTable::HashTable(){
+    //...
+}
+
+void HashTable::insert( Entry* entry ) {
+    Entry& newEntry = *entry;
+    string id = newEntry[ Entry::ID ];
+    long idi = stol( id );
+
+    long hash = idi % prime;
+    ++counters[hash];
+}
+
+void HashTable::report(  ) {
+    int total = 0;
+    for ( int i = 0; i < prime; ++i ) {
+        if ( counters[i] != 0 ) {
+            std::cout << i << ": " << counters[i] << std::endl;
+            total += counters[i];
+        }
+    }
+
+    std::cout << total << std::endl;
+}
