9173b9a1b6b3bc58bc1c9b03ecaa105c989a2e53
devpin <devpindroid@gmail.com>
2017-04-12 21:35:00 -0600

    Initial commit

--- HashTable.h
+++ HashTable.h
@@ -6,8 +6,15 @@
 #define HW9_HASHTABLE_H
 
 #include "Entry.h"
+#include <list>
+#include <stdio.h>
 
 class HashTable {
+public:
+    HashTable();
+    void insert( const Entry* );
+private:
+    std::list<Entry*> buckets[4111];
 
 };
 
