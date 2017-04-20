781ea59b7f22a1cf637191bc8d9f5b6281a8b3c9
devpin <devpindroid@gmail.com>
2017-04-20 16:42:48 -0600

    Implemented member functions

--- Table.cpp
+++ Table.cpp
@@ -1,6 +1,15 @@
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 //
-// Created by devin on 4/1/2017.
+//                                            Table.h
+//                                    Devin Piner - 107543409
+//                                         April 1, 2017
 //
+//                                Interface for the Table class
+//                  Hold data in a list so that it can be accessed with subscripts
+//
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
 #include "Table.h"
 
@@ -8,14 +17,13 @@ Table::Table( vector<string> ) {
 
 }
 
-bool Table::insert( const Entry& ) { //function to insert and entry into the table
+bool Table::insert( Entry* entry ) { //function to insert and entry into the table
+    data.push_back( entry );
+    ++size;
 
     return false;
 }
 
-Entry& Table::operator[]( int ) {
-
-    Entry* entry;
-
-    return *(entry);
+Entry* Table::operator[]( int i ) {
+    return data[i];
 }
