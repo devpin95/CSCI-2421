7334b1f7a502e89e44b2f540e8936c224b3ae5a7
devpin <devpindroid@gmail.com>
2017-04-20 16:37:46 -0600

    Added member function stubs

--- Table.cpp
+++ Table.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Table.h"
+
+Table::Table( vector<string> ) {
+
+}
+
+bool Table::insert( const Entry& ) { //function to insert and entry into the table
+
+    return false;
+}
+
+Entry& Table::operator[]( int ) {
+
+    Entry* entry;
+
+    return *(entry);
+}
